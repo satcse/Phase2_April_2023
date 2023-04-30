ebf5be69bd82babc77bdc3df0a4db5e65ea117b8
satcse <sathish.tipofeverest@gmail.com>
2023-04-30 23:28:54 +0530

    Day 8 commit

---
+++ HibernateXML/src/com/ecommerce/HibernateUtil.java
@@ -0,0 +1,32 @@
+package com.ecommerce;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+	
+	private static final SessionFactory sessionFactory;
+	
+	static
+	{
+		try
+		{
+			StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+			Metadata metadata = new MetadataSources(standardRegistry).getMetadataBuilder().build();
+			
+			sessionFactory = metadata.getSessionFactoryBuilder().build();			
+		}catch(Throwable th)
+		{
+			throw new ExceptionInInitializerError(th);
+		}
+	}
+	
+	public static SessionFactory getSessionFactory()
+	{
+		return sessionFactory;
+	}
+
+}
